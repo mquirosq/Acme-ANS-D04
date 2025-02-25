697fcf40992aada114fa12e2f313e1bff90c1a3d
Ignacio Mora <ignaciomorap04@gmail.com>
2025-02-25 16:49:21 +0100

    Created initial version of Airport entity and AirportScope datatype. #151

---
+++ src/main/java/acme/datatypes/AirportScope.java
@@ -0,0 +1,8 @@
+
+package acme.datatypes;
+
+public enum AirportScope {
+
+	INTERNATIONAL, DOMESTIC, REGIONAL
+
+}
---
+++ src/main/java/acme/entities/Airport.java
@@ -0,0 +1,69 @@
+
+package acme.entities;
+
+import javax.persistence.Entity;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidEmail;
+import acme.client.components.validation.ValidString;
+import acme.client.components.validation.ValidUrl;
+import acme.datatypes.AirportScope;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Airport extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	@ValidString(max = 50)
+	@Automapped
+	private String				name;
+
+	@Mandatory
+	@ValidString(pattern = "^[A-Z]{3}$")
+	@Automapped
+	private String				IATA;
+
+	@Mandatory
+	@Valid
+	@Automapped
+	private AirportScope		scope;
+
+	@Mandatory
+	@ValidString(max = 50)
+	@Automapped
+	private String				city;
+
+	@Mandatory
+	@ValidString(max = 50)
+	@Automapped
+	private String				country;
+
+	@Optional
+	@ValidUrl
+	@Automapped
+	private String				website;
+
+	@Optional
+	@ValidEmail
+	@Automapped
+	private String				email;
+
+	@Optional
+	@Automapped
+	private String				address;
+
+	@Optional
+	@ValidString(pattern = "^\\+?\\d{6,15}$")
+	@Automapped
+	private String				phoneNumber;
+
+}
