417622cb5a38941c4c69720042ae69086b4bc64d
Guillermo Rodríguez Narbona <guillermo.rn04@gmail.com>
2025-02-25 16:55:36 +0100

    Implemented the Aircraft entity #153

---
+++ src/main/java/acme/entities/Aircraft.java
@@ -0,0 +1,55 @@
+
+package acme.entities;
+
+import javax.persistence.Entity;
+import javax.validation.Valid;
+
+import org.checkerframework.common.aliasing.qual.Unique;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Aircraft extends AbstractEntity {
+
+	public static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	@Automapped
+	@ValidString(max = 50)
+	private String				model;
+
+	@Unique
+	@Mandatory
+	@Automapped
+	@ValidString(max = 50)
+	private String				registrationNumber;
+
+	@Mandatory
+	@Automapped
+	private Integer				capacity;
+
+	@Mandatory
+	@Automapped
+	@ValidNumber(min = 2000, max = 50000)
+	private Integer				cargoWeight;
+
+	@Valid
+	@Mandatory
+	@Automapped
+	private AircraftStatus		status;
+
+	@Optional
+	@Automapped
+	@ValidString(max = 255)
+	private String				details;
+
+}
---
+++ src/main/java/acme/entities/AircraftStatus.java
@@ -0,0 +1,6 @@
+
+package acme.entities;
+
+public enum AircraftStatus {
+	IN_ACTIVE_SERVICE, UNDER_MAINTENANCE
+}
