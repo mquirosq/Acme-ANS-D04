98259c135494f13bc91a8297e8ef331b2dc7b29f
mquirosq <mquirosquiroga@gmail.com>
2025-02-27 19:56:22 +0100

    Create Banned Passenger Entity #160

---
+++ src/main/java/acme/entities/BannedPassenger.java
@@ -0,0 +1,61 @@
+
+package acme.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidString;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class BannedPassenger extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	@ValidString(max = 50)
+	@Automapped
+	private String				fullName;
+
+	@Mandatory
+	@ValidMoment(past = true)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				dateOfBirth;
+
+	@Mandatory
+	@ValidString(pattern = "^[A-Z0-9]{6,9}$")
+	@Automapped
+	private String				passportNumber;
+
+	@Mandatory
+	@ValidString(max = 50)
+	@Automapped
+	private String				nationality;
+
+	@Mandatory
+	@ValidString(max = 255)
+	@Automapped
+	private String				reason;
+
+	@Mandatory
+	@ValidMoment(past = true)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				issuedAt;
+
+	@Optional
+	@ValidMoment(past = true)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				liftedAt;
+
+}
