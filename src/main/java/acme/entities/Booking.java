962cc765716c990e78719ae9ffc0a4f24a0354fc
mquirosq <mquirosquiroga@gmail.com>
2025-02-27 21:13:12 +0100

    Create Booking entity #167

---
+++ src/main/java/acme/entities/Booking.java
@@ -0,0 +1,52 @@
+
+package acme.entities;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.datatypes.Money;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidMoney;
+import acme.client.components.validation.ValidString;
+import acme.datatypes.TravelClass;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Booking extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	@ValidString(pattern = "^[A-Z0-9]{6,8}$")
+	@Column(unique = true)
+	private String				locatorCode;
+
+	@Mandatory
+	@ValidMoment(past = true)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				purchasedAt;
+
+	@Mandatory
+	@Valid
+	@Automapped
+	private TravelClass			type;
+
+	@Mandatory
+	@ValidMoney
+	@Automapped
+	private Money				price;
+
+	// TODO: Relationship with flight
+
+}
