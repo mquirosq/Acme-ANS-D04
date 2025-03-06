bad1f52e82e1b6084f78819083311ecaafa0da76
Daniel Herrera <danielherreraurbano2004@gmail.com>
2025-03-06 11:57:50 +0100

    Add Service entity

---
+++ src/main/java/acme/entities/Service.java
@@ -0,0 +1,59 @@
+
+package acme.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.datatypes.Money;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoney;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
+import acme.client.components.validation.ValidUrl;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Service extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	@ValidString(max = 50)
+	@Automapped
+	private String				name;
+
+	@Mandatory
+	@ValidUrl
+	@Automapped
+	private String				pictureLink;
+
+	@Mandatory
+	@ValidNumber
+	@Automapped
+	private Integer				avgDwellTime;
+
+	@Optional
+	@ValidString(pattern = "^[A-Z]{4}-[0-9]{2}$")
+	@Column(unique = true)
+	private String				promotionCode;
+
+	@Optional
+	@ValidMoney
+	@Automapped
+	private Money				discountMoney;
+
+	// Relationships ---------------------------------------------------------------------
+
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private Airport				airport;
+}
