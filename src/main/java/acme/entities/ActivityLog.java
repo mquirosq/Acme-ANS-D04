252b9770613656875db6901eb20f59b3c14c6118
Daniel Herrera <danielherreraurbano2004@gmail.com>
2025-03-08 04:03:22 +0100

    Add ActivityLog entity
    Relationship with Leg and validations still pending

---
+++ src/main/java/acme/entities/ActivityLog.java
@@ -0,0 +1,56 @@
+
+package acme.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class ActivityLog extends AbstractEntity {
+
+	public static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	@ValidMoment
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				registrationMoment;
+
+	@Mandatory
+	@ValidString(max = 50)
+	@Automapped
+	private String				typeOfIncident;
+
+	@Mandatory
+	@ValidString(max = 255)
+	@Automapped
+	private String				description;
+
+	@Mandatory
+	@ValidNumber(min = 0, max = 10)
+	private Integer				severityLevel;
+
+	// Relationships -------------------------------------------------------
+
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private FlightCrewMember	logger;
+
+	// TODO: Include Many To One relationship with Leg
+
+}
