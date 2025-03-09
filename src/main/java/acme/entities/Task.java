a79eecb26e4a7990ade4e70fea0f45eac25344a9
Alejandro Parody <alejandroparodyq@gmail.com>
2025-03-09 18:30:24 +0100

    Added task entity #227

---
+++ src/main/java/acme/entities/Task.java
@@ -0,0 +1,47 @@
+
+package acme.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.ValidNumber;
+import acme.datatypes.TaskType;
+import acme.realms.Technician;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Task {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private Technician			technician;
+
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private Aircraft			aircraft;
+
+	@Mandatory
+	@Valid
+	@Automapped
+	private TaskType			type;
+
+	@Mandatory
+	@ValidNumber(min = 0, max = 10)
+	@Automapped
+	private Integer				priority;
+
+	@Mandatory
+	@ValidNumber(min = 0)
+	@Automapped
+	private Integer				hourEstimate;
+}
