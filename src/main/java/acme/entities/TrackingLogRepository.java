7e1d59d49d9370e582a05c017c8f6da4a3a7c9ad
Guillermo Rodríguez Narbona <guillermo.rn04@gmail.com>
2025-03-12 11:11:26 +0100

    Solved errors in tracking log validation #368

--- src/main/java/acme/entities/TrackingLogRepository.java
+++ src/main/java/acme/entities/TrackingLogRepository.java
@@ -1,6 +1,7 @@
 
 package acme.entities;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
@@ -11,6 +12,6 @@ import acme.client.repositories.AbstractRepository;
 @Repository
 public interface TrackingLogRepository extends AbstractRepository {
 
-	@Query("select t from TrackingLog t where t.claim.id = :claimId order by t.lastUpdateMoment desc")
-	List<TrackingLog> findAllByClaimId(Integer claimId);
+	@Query("select t from TrackingLog t where t.claim.id = :claimId and t.id != :trackingLogId and t.lastUpdateMoment < :lastUpdateMoment order by t.lastUpdateMoment desc, t.resolutionPercentage desc")
+	List<TrackingLog> findAllByClaimIdWithDifferentIdBefore(Integer claimId, Integer trackingLogId, Date lastUpdateMoment);
 }
