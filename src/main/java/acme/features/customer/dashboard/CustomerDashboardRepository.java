b84fcccd9caa9080596f40c0d0eb704ff0aab93b
mquirosq <mquirosquiroga@gmail.com>
2025-03-18 21:45:09 +0100

    Add repository query for standard deviation #171

--- src/main/java/acme/features/customer/dashboard/CustomerDashboardRepository.java
+++ src/main/java/acme/features/customer/dashboard/CustomerDashboardRepository.java
@@ -5,44 +5,46 @@ import java.util.Collection;
 import java.util.Date;
 
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import acme.client.repositories.AbstractRepository;
 
 public interface CustomerDashboardRepository extends AbstractRepository {
 
 	@Query("select b.flight from Booking b where b.customer.id = :customerId order by b.purchasedAt desc")
-	Collection<String> getFlightsOrderByRecentBooking(@Param("customerId") Integer customerId);
+	Collection<String> getFlightsOrderByRecentBooking(Integer customerId);
 
 	@Query("select b.travelClass, count(b) from Booking b where b.customer.id = :customerId group by b.travelClass")
-	Collection<Object[]> getNumberOfBookingsGroupedByTravelClass(@Param("customerId") Integer customerId);
+	Collection<Object[]> getNumberOfBookingsGroupedByTravelClass(Integer customerId);
 
 	@Query("select sum(b.price.amount) from Booking b where b.purchasedAt >= :startDate")
-	Double getTotalCostOfBookingsSinceDate(@Param("startDate") Date startDate);
+	Double getTotalCostOfBookingsSinceDate(Date startDate);
 
 	@Query("select avg(b.price.amount) from Booking b where b.purchasedAt >= :startDate")
-	Double getAverageCostOfBookingsSinceDate(@Param("startDate") Date startDate);
+	Double getAverageCostOfBookingsSinceDate(Date startDate);
 
 	@Query("select min(b.price.amount) from Booking b where b.purchasedAt >= :startDate")
-	Double getMinimumCostOfBookingsSinceDate(@Param("startDate") Date startDate);
+	Double getMinimumCostOfBookingsSinceDate(Date startDate);
 
 	@Query("select max(b.price.amount) from Booking b where b.purchasedAt >= :startDate")
-	Double maximumCostOfBookingsSinceDate(@Param("startDate") Date startDate);
+	Double maximumCostOfBookingsSinceDate(Date startDate);
 
 	@Query("select stddev(b.price.amount) from Booking b where b.purchasedAt >= :startDate")
-	Double standardDeviationCostOfBookingsSinceDate(@Param("startDate") Date startDate);
+	Double standardDeviationCostOfBookingsSinceDate(Date startDate);
 
 	@Query("select count(br) from BookingRecord br where br.booking.customer=:customerId")
-	Long getTotalPassengersInBookings(@Param("customerId") Integer customerId);
+	Long getTotalPassengersInBookings(Integer customerId);
 
 	@Query("select avg(select count(br) from BookingRecord br where br.booking.id = b.id) from Booking b where b.customer.id = :customerId")
-	Double getAverageNumberOfPassengersInBooking(@Param("customerId") Integer customerId);
+	Double getAverageNumberOfPassengersInBooking(Integer customerId);
 
 	@Query("select min(select count(br) from BookingRecord br where br.booking.id = b.id) from Booking b where b.customer.id = :customerId")
-	Long getMinimumNumberOfPassengersInBooking(@Param("customerId") Integer customerId);
+	Long getMinimumNumberOfPassengersInBooking(Integer customerId);
 
 	@Query("select max(select count(br) from BookingRecord br where br.booking.id = b.id) from Booking b where b.customer.id = :customerId")
-	Long getMaximumNumberOfPassengersInBooking(@Param("customerId") Integer customerId);
+	Long getMaximumNumberOfPassengersInBooking(Integer customerId);
 
 	// Standard deviation number of passengers in bookings
+	@Query("select count(br) from BookingRecord br group by br.booking.id")
+	Collection<Long> getPassengersByBooking();
+
 }
