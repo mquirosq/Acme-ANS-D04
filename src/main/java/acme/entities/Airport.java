4fb733bdff2707119f84feb963a0872f143cb4ce
Ignacio Mora <ignaciomorap04@gmail.com>
2025-03-18 20:50:44 +0100

    added validation to check airports' IATA codes are unique. #515

--- src/main/java/acme/entities/Airport.java
+++ src/main/java/acme/entities/Airport.java
@@ -12,6 +12,7 @@ import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidEmail;
 import acme.client.components.validation.ValidString;
 import acme.client.components.validation.ValidUrl;
+import acme.constraints.ValidAirport;
 import acme.datatypes.AirportScope;
 import lombok.Getter;
 import lombok.Setter;
@@ -19,6 +20,7 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
+@ValidAirport
 public class Airport extends AbstractEntity {
 
 	private static final long	serialVersionUID	= 1L;
