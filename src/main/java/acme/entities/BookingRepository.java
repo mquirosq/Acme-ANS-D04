f912f3cb0f16cd3d1ddb6a151a9b5b3428e50a1b
mquirosq <mquirosquiroga@gmail.com>
2025-03-30 13:12:29 +0200

    Add draft mode for bookings #593

--- src/main/java/acme/entities/BookingRepository.java
+++ src/main/java/acme/entities/BookingRepository.java
@@ -7,6 +7,9 @@ import acme.client.repositories.AbstractRepository;
 
 public interface BookingRepository extends AbstractRepository {
 
-	@Query("select b from Booking b where locatorCode = :locatorCode")
+	@Query("select b from Booking b where b.locatorCode = :locatorCode")
 	Booking getSameLocatorCode(String locatorCode);
+
+	@Query("select count(br) from BookingRecord br where br.booking.id = :bookingId")
+	Long getNumberOfPassengers(int bookingId);
 }
