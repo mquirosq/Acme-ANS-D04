f2c6d0a92ef3373a9e5a4baf97e974b646bb2249
mquirosq <mquirosquiroga@gmail.com>
2025-03-31 11:35:20 +0200

    Add support for create and edit in views

--- src/main/java/acme/features/customer/booking/CustomerBookingShowService.java
+++ src/main/java/acme/features/customer/booking/CustomerBookingShowService.java
@@ -58,7 +58,7 @@ public class CustomerBookingShowService extends AbstractGuiService<Customer, Boo
 		flightChoices = SelectChoices.from(flights, "id", booking.getFlight());
 		travelChoices = SelectChoices.from(TravelClass.class, booking.getTravelClass());
 
-		dataset = super.unbindObject(booking, "locatorCode", "travelClass", "lastCardNibble", "price", "purchasedAt");
+		dataset = super.unbindObject(booking, "locatorCode", "travelClass", "lastCardNibble", "price", "purchasedAt", "draftMode");
 		dataset.put("flight", flightChoices.getSelected().getKey());
 		dataset.put("flights", flightChoices);
 		dataset.put("travelClass", travelChoices.getSelected().getKey());
