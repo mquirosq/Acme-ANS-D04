e00390de295dd6908add126ee861a1876c9d09de
Ignacio Mora <ignaciomorap04@gmail.com>
2025-03-31 13:50:30 +0200

    Added use of MomentHelper and removed repeated validation. #566

--- src/main/java/acme/constraints/FlightLegValidator.java
+++ src/main/java/acme/constraints/FlightLegValidator.java
@@ -1,9 +1,6 @@
 
 package acme.constraints;
 
-import java.util.Date;
-import java.util.List;
-
 import javax.validation.ConstraintValidatorContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,22 +41,9 @@ public class FlightLegValidator extends AbstractValidator<ValidFlightLeg, Flight
 				super.state(context, firstCharsFromIATA, "flightNumber", "acme.validation.flightLeg.flightNumber.message");
 			}
 			if (leg.getScheduledDeparture() != null && leg.getScheduledArrival() != null) {
-				boolean departureIsBeforeArrival = leg.getScheduledDeparture().compareTo(leg.getScheduledArrival()) < 0;
+				boolean departureIsBeforeArrival = MomentHelper.isBefore(leg.getScheduledDeparture(), leg.getScheduledArrival());
 
 				super.state(context, departureIsBeforeArrival, "dates", "acme.validation.flightLeg.scheduledDates.message");
-			}
-			if (leg.getParentFlight() != null) {
-				boolean isNotOverlapping = true;
-
-				List<FlightLeg> legsOfFlight = this.repository.getLegsOfFlight(leg.getParentFlight().getId());
-
-				for (Integer i = 0; i < legsOfFlight.size() - 1; i++) {
-					Date scheduledArrivalFirst = legsOfFlight.get(i).getScheduledArrival();
-					Date scheduledDepartureSecond = legsOfFlight.get(i + 1).getScheduledDeparture();
-					if (MomentHelper.isAfterOrEqual(scheduledArrivalFirst, scheduledDepartureSecond))
-						isNotOverlapping = false;
-				}
-				super.state(context, isNotOverlapping, "dates", "acme.validation.flight.overlappingDates.message");
 			}
 		}
 
