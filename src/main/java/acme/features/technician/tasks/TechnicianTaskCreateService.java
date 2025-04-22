843e4f98368bb67297dfb7781b071a2d10c3866f
AlejandroParody <alejandroparodyq@gmail.com>
2025-04-22 17:45:33 +0200

    Added operations over tasks #501

---
+++ src/main/java/acme/features/technician/tasks/TechnicianTaskCreateService.java
@@ -0,0 +1,71 @@
+
+package acme.features.technician.tasks;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.client.components.models.Dataset;
+import acme.client.components.views.SelectChoices;
+import acme.client.services.AbstractGuiService;
+import acme.client.services.GuiService;
+import acme.datatypes.TaskType;
+import acme.entities.Task;
+import acme.realms.Technician;
+
+@GuiService
+public class TechnicianTaskCreateService extends AbstractGuiService<Technician, Task> {
+
+	@Autowired
+	private TechnicianTaskRepository repository;
+
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		Task task;
+		Technician technician;
+
+		technician = (Technician) super.getRequest().getPrincipal().getActiveRealm();
+
+		task = new Task();
+		task.setIsDraft(true);
+		task.setTechnician(technician);
+
+		super.getBuffer().addData(task);
+	}
+
+	@Override
+	public void bind(final Task task) {
+		Technician technician = (Technician) super.getRequest().getPrincipal().getActiveRealm();
+
+		super.bindObject(task, "type", "description", "priority", "hourEstimate");
+		task.setTechnician(technician);
+	}
+
+	@Override
+	public void validate(final Task task) {
+
+	}
+
+	@Override
+	public void perform(final Task task) {
+		this.repository.save(task);
+	}
+
+	@Override
+	public void unbind(final Task task) {
+		Dataset dataset;
+		SelectChoices typeChoices;
+
+		typeChoices = SelectChoices.from(TaskType.class, task.getType());
+
+		dataset = super.unbindObject(task, "description", "priority", "hourEstimate");
+		dataset.put("types", typeChoices);
+		dataset.put("type", typeChoices.getSelected().getKey());
+
+		super.getResponse().addData(dataset);
+	}
+}
