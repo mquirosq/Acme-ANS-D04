814c2c1a80d4c1d5cd19aa32c467e317b72f8d99
Ignacio Mora <ignaciomorap04@gmail.com>
2025-04-22 15:08:01 +0200

    Implemented flight leg details showing. #774

--- src/main/java/acme/features/airlineManager/flightLeg/AirlineManagerFlightLegRepository.java
+++ src/main/java/acme/features/airlineManager/flightLeg/AirlineManagerFlightLegRepository.java
@@ -7,8 +7,11 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import acme.client.repositories.AbstractRepository;
+import acme.entities.Aircraft;
+import acme.entities.Airport;
 import acme.entities.Flight;
 import acme.entities.FlightLeg;
+import acme.realms.AirlineManager;
 
 @Repository
 public interface AirlineManagerFlightLegRepository extends AbstractRepository {
@@ -18,5 +21,20 @@ public interface AirlineManagerFlightLegRepository extends AbstractRepository {
 
 	@Query("select f from Flight f where f.id = :flightId")
 	Flight findFlightById(int flightId);
+
+	@Query("select fl from FlightLeg fl where fl.id = :legId")
+	FlightLeg findFlightLegById(int legId);
+
+	@Query("select am from AirlineManager am where am.id = :managerId")
+	AirlineManager findManagerById(int managerId);
+
+	@Query("select f from Flight f where f.manager.id = :airlineManagerId")
+	Collection<Flight> findAllFlightsByAirlineManagerId(int airlineManagerId);
+
+	@Query("select a from Airport a")
+	Collection<Airport> findAllAirports();
+
+	@Query("select a from Aircraft a")
+	Collection<Aircraft> findAllAircrafts();
 
 }
--- src/main/java/acme/features/airlineManager/flightLeg/AirlineManagerFlightLegShowService.java
+++ src/main/java/acme/features/airlineManager/flightLeg/AirlineManagerFlightLegShowService.java
@@ -1,10 +1,18 @@
 
 package acme.features.airlineManager.flightLeg;
 
+import java.util.Collection;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
+import acme.client.components.models.Dataset;
+import acme.client.components.views.SelectChoices;
 import acme.client.services.AbstractGuiService;
 import acme.client.services.GuiService;
+import acme.datatypes.FlightLegStatus;
+import acme.entities.Aircraft;
+import acme.entities.Airport;
+import acme.entities.Flight;
 import acme.entities.FlightLeg;
 import acme.realms.AirlineManager;
 
@@ -14,44 +22,71 @@ public class AirlineManagerFlightLegShowService extends AbstractGuiService<Airli
 	@Autowired
 	private AirlineManagerFlightLegRepository repository;
 
-	/*
-	 * 
-	 * @Override
-	 * public void authorise() {
-	 * super.getResponse().setAuthorised(true);
-	 * }
-	 * 
-	 * @Override
-	 * public void load() {
-	 * Flight flight;
-	 * int id;
-	 * 
-	 * id = super.getRequest().getData("id", int.class);
-	 * flight = this.repository.findFlightById(id);
-	 * 
-	 * super.getBuffer().addData(flight);
-	 * }
-	 * 
-	 * @Override
-	 * public void unbind(final Flight flight) {
-	 * Dataset dataset;
-	 * Date scheduledDeparture = flight.getScheduledDeparture();
-	 * Date scheduledArrival = flight.getScheduledArrival();
-	 * String originCity = flight.getOriginCity();
-	 * String destinationCity = flight.getDestinationCity();
-	 * Integer numberOfLayovers = flight.getNumberOfLayovers();
-	 * 
-	 * dataset = super.unbindObject(flight, "tag", "requiresSelfTransfer", "cost", "description", "draftMode");
-	 * 
-	 * dataset.put("scheduledDeparture", scheduledDeparture != null ? scheduledDeparture : "-");
-	 * dataset.put("scheduledArrival", scheduledArrival != null ? scheduledArrival : "-");
-	 * dataset.put("originCity", originCity != null ? originCity : "-");
-	 * dataset.put("destinationCity", destinationCity != null ? destinationCity : "-");
-	 * dataset.put("numberOfLayovers", numberOfLayovers);
-	 * 
-	 * super.getResponse().addData(dataset);
-	 * }
-	 * 
-	 */
+
+	@Override
+	public void authorise() {
+		boolean authorised;
+
+		int legId, managerId;
+		FlightLeg leg;
+		AirlineManager manager;
+
+		legId = super.getRequest().getData("id", int.class);
+		leg = this.repository.findFlightLegById(legId);
+
+		managerId = super.getRequest().getPrincipal().getActiveRealm().getId();
+		manager = this.repository.findManagerById(managerId);
+
+		authorised = leg != null && leg.getParentFlight() != null && leg.getParentFlight().getManager() != null && leg.getParentFlight().getManager().equals(manager);
+
+		super.getResponse().setAuthorised(authorised);
+	}
+
+	@Override
+	public void load() {
+		FlightLeg leg;
+		int id;
+
+		id = super.getRequest().getData("id", int.class);
+		leg = this.repository.findFlightLegById(id);
+
+		super.getBuffer().addData(leg);
+	}
+
+	@Override
+	public void unbind(final FlightLeg leg) {
+		Dataset dataset;
+		Collection<Flight> flights;
+		Collection<Airport> airports;
+		Collection<Aircraft> aircrafts;
+
+		SelectChoices statusChoices, arrivalAirportChoices, departureAirportChoices, aircraftChoices, flightChoices;
+
+		int managerId = super.getRequest().getPrincipal().getActiveRealm().getId();
+		flights = this.repository.findAllFlightsByAirlineManagerId(managerId);
+		airports = this.repository.findAllAirports();
+		aircrafts = this.repository.findAllAircrafts();
+
+		statusChoices = SelectChoices.from(FlightLegStatus.class, leg.getStatus());
+		arrivalAirportChoices = SelectChoices.from(airports, "IATACode", leg.getArrivalAirport());
+		departureAirportChoices = SelectChoices.from(airports, "IATACode", leg.getDepartureAirport());
+		aircraftChoices = SelectChoices.from(aircrafts, "registrationNumber", leg.getDeployedAircraft());
+		flightChoices = SelectChoices.from(flights, "tag", leg.getParentFlight());
+
+		dataset = super.unbindObject(leg, "flightNumber", "scheduledDeparture", "scheduledArrival", "draftMode");
+
+		dataset.put("parentFlights", flightChoices);
+		dataset.put("parentFlight", flightChoices.getSelected().getKey());
+		dataset.put("statuses", statusChoices);
+		dataset.put("status", statusChoices.getSelected().getKey());
+		dataset.put("departureAirports", departureAirportChoices);
+		dataset.put("departureAirport", departureAirportChoices.getSelected().getKey());
+		dataset.put("arrivalAirports", arrivalAirportChoices);
+		dataset.put("arrivalAirport", arrivalAirportChoices.getSelected().getKey());
+		dataset.put("deployedAircrafts", aircraftChoices);
+		dataset.put("deployedAircraft", aircraftChoices.getSelected().getKey());
+
+		super.getResponse().addData(dataset);
+	}
 
 }
