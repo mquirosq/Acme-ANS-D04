ab59c520b997d9fcd546cff78898a4a1a632d132
Ignacio Mora <ignaciomorap04@gmail.com>
2025-05-19 01:11:26 +0200

    Made modifiable properties actually modifiable in Flight publish feature. #886

--- src/main/java/acme/features/airlineManager/flight/AirlineManagerFlightPublishService.java
+++ src/main/java/acme/features/airlineManager/flight/AirlineManagerFlightPublishService.java
@@ -48,7 +48,7 @@ public class AirlineManagerFlightPublishService extends AbstractGuiService<Airli
 
 	@Override
 	public void bind(final Flight flight) {
-		;
+		super.bindObject(flight, "tag", "requiresSelfTransfer", "cost", "description");
 	}
 
 	@Override
