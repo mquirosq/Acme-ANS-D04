48727a452e9b2e79b76652508590de8c6d0b9fae
Alejandro Parody <alejandroparodyq@gmail.com>
2025-05-26 10:49:12 +0200

    implemented signup as technician #503

---
+++ src/main/java/acme/features/authenticated/technician/AuthenticatedTechnicianController.java
@@ -0,0 +1,24 @@
+
+package acme.features.authenticated.technician;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.client.components.principals.Authenticated;
+import acme.client.controllers.AbstractGuiController;
+import acme.client.controllers.GuiController;
+import acme.realms.Technician;
+
+@GuiController
+public class AuthenticatedTechnicianController extends AbstractGuiController<Authenticated, Technician> {
+
+	@Autowired
+	private AuthenticatedTechnicianCreateService createService;
+
+
+	@PostConstruct
+	protected void initialise() {
+		super.addBasicCommand("create", this.createService);
+	}
+}
